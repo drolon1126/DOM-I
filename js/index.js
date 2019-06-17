b0a754dda8875a71f267ef37fbdea2f50cfb07b3
danRolon <daniel.rolon@yahoo.com>
2019-06-17 14:34:04 -0500

    added main stuff

--- js/index.js
+++ js/index.js
@@ -70,3 +70,20 @@ ctaH1.textContent = siteContent['cta']['h1'];
 
 let ctaBttn = document.querySelector('.cta-text button');
 ctaBttn.textContent = siteContent['cta']['button'];
+
+/* ~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~ */
+
+let mainH4 = document.querySelectorAll('.text-content h4');
+mainH4[0].textContent = siteContent['main-content']['features-h4'];
+mainH4[1].textContent = siteContent['main-content']['about-h4'];
+mainH4[2].textContent = siteContent['main-content']['services-h4'];
+mainH4[3].textContent = siteContent['main-content']['product-h4'];
+mainH4[4].textContent = siteContent['main-content']['vision-h4'];
+
+let mainP = document.querySelectorAll('.text-content p');
+mainP[0].textContent = siteContent['main-content']['features-content'];
+mainP[1].textContent = siteContent['main-content']['about-content'];
+mainP[2].textContent = siteContent['main-content']['services-content'];
+mainP[3].textContent = siteContent['main-content']['product-content'];
+mainP[4].textContent = siteContent['main-content']['vision-content'];
+
