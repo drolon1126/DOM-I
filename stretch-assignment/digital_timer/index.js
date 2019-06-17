c6c28bbd8bdc38324af7db4b4e8fd37a334da2ea
danRolon <daniel.rolon@yahoo.com>
2019-06-17 15:04:58 -0500

    added functions to timer to start stop and update the timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -34,3 +34,86 @@ body.appendChild(startBtn);
 body.appendChild(resetBtn);
 
 let keepTime;
+
+//Functions
+
+//start the timer
+function start(){
+  isRunning = true;
+  keepTime = setInterval(timer, 10);
+  startBtn.style.display='none';
+  pauseBtn.style.display='inline-block';
+
+}
+
+//keep track of the digits so they display correctly
+function updateDigits(){
+  digits[0].textContent = secondTens.toString();
+  digits[1].textContent = secondOnes.toString();
+  digits[3].textContent = msTens.toString();
+  digits[4].textContent = msHundreds.toString();
+}
+
+function timer(){
+  if(secondTens===1){
+    clearInterval(keepTime);
+    digits.forEach(dig=>dig.style.color='red');
+    digits[2].style.color='black';
+    pauseBtn.style.display='none';
+  } else{
+    addMsHundreds();
+  }
+  updateDigits();
+}
+
+//Add numbers to the digits for updating
+function addMsHundreds(){
+  msHundreds++;
+  if(msHundreds>9){
+    msHundreds = 0;
+    addMsTens();
+  }
+}
+function addMsTens(){
+  msTens++;
+  if(msTens>9){
+    msTens = 0;
+    addSecOnes();
+  }
+
+}
+function addSecOnes(){
+  secondOnes++;
+  if(secondOnes>9){
+    secondOnes = 0;
+    addSecTen();
+  }
+}
+function addSecTen(){
+  secondTens++;
+}
+
+//pause and resume the timer
+function pause(){
+  if(isRunning){
+    clearInterval(keepTime);
+    pauseBtn.textContent="RESUME";
+  }else{
+    keepTime = setInterval(timer, 10);
+    pauseBtn.textContent="PAUSE";
+  }
+  isRunning = !isRunning;
+}
+
+//reset back to 0
+function reset(){
+  clearInterval(keepTime);
+  secondTens= 0;
+  secondOnes= 0;
+  msHundreds= 0;
+  msTens= 0;
+  digits.forEach(dig=>dig.style.color='black');
+  updateDigits();
+  startBtn.style.display='inline-block';
+  pauseBtn.style.display='none';
+}
